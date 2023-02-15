6d21f189e7466f75a26474a24d0ae2c705618f73
reamkf <68076678+reamkf@users.noreply.github.com>
2023-02-15 19:50:02 +0900

    Rename some variables

--- src/extension.ts
+++ src/extension.ts
@@ -33,43 +33,43 @@ const docSel : vscode.DocumentSelector = { language: "seesaawiki", scheme: "file
 class DocumentSymbolProvider {
 	provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken){
 		const documentSymbols : vscode.DocumentSymbol[] = [];
-		let ds : vscode.DocumentSymbol;
-		const heading : Array<vscode.DocumentSymbol | null> = [null, null, null];
+		let symbol : vscode.DocumentSymbol;
+		const lastUnclosedHeadingSymbol : Array<vscode.DocumentSymbol | null> = [null, null, null];
 
-		for(let i = 0; i < document.lineCount; i++){
-			const line = document.lineAt(i);
+		for(let lineNum = 0; lineNum < document.lineCount; lineNum++){
+			const line = document.lineAt(lineNum);
 			const headingMatch = line.text.match(/^\*{0,3}/);
 			const headingLevel = headingMatch && headingMatch[0].length || 0;
 			if(headingLevel){
-				ds = new vscode.DocumentSymbol(line.text, "Heading " + String(headingLevel), vscode.SymbolKind.String, line.range, line.range);
+				symbol = new vscode.DocumentSymbol(line.text, "Heading " + String(headingLevel), vscode.SymbolKind.String, line.range, line.range);
 
 				// Close opened deeper headings
-				for(let j = headingLevel; j <= 3; j++){
-					if(heading[j-1] !== null){
-						heading[j-1]!.range = new vscode.Range(heading[j-1]?.range.start || new vscode.Position(i-1, 0), new vscode.Position(i-1, 0));
-						heading[j-1] = null;
+				for(let level = headingLevel; level <= 3; level++){
+					if(lastUnclosedHeadingSymbol[level-1] !== null){
+						lastUnclosedHeadingSymbol[level-1]!.range = new vscode.Range(lastUnclosedHeadingSymbol[level-1]?.range.start || new vscode.Position(lineNum-1, 0), new vscode.Position(lineNum-1, 0));
+						lastUnclosedHeadingSymbol[level-1] = null;
 					}
 				}
 
 				// append child or push to documentSymbols
 				let childFlag = false;
 				for(let j = headingLevel-1; j > 0; j--){
-					if(heading[j-1] !== null){
-						heading[j-1]!.children.push(ds);
+					if(lastUnclosedHeadingSymbol[j-1] !== null){
+						lastUnclosedHeadingSymbol[j-1]!.children.push(symbol);
 						childFlag = true;
 						break;
 					}
 				}
 				if(!childFlag){
-					documentSymbols.push(ds);
+					documentSymbols.push(symbol);
 				}
-				heading[headingLevel-1] = ds;
+				lastUnclosedHeadingSymbol[headingLevel-1] = symbol;
 			}
 		}
 		const lastLine = document.lineAt(document.lineCount-1);
 		for(const level of [1, 2, 3]){
-			if(heading[level-1] !== null){
-				heading[level-1]!.range = new vscode.Range(heading[level-1]?.range.start || new vscode.Position(document.lineCount-1, 0), lastLine.range.start);
+			if(lastUnclosedHeadingSymbol[level-1] !== null){
+				lastUnclosedHeadingSymbol[level-1]!.range = new vscode.Range(lastUnclosedHeadingSymbol[level-1]?.range.start || new vscode.Position(document.lineCount-1, 0), lastLine.range.start);
 			}
 		}
 
@@ -80,47 +80,49 @@ class DocumentSymbolProvider {
 class FoldingRangeProvider {
 	provideFoldingRanges(document: vscode.TextDocument, context: vscode.FoldingContext, token: vscode.CancellationToken){
 		const foldingRanges : vscode.FoldingRange[] = [];
-		const headingStartLine : number[] = [-1, -1, -1];
-		const foldingStartLine : number[] = [];
-		const headingInFolding : number[] = [-1, -1, -1];
+		const headingStartLineNum : number[] = [-1, -1, -1];
+		const foldingStartLineNum : number[] = [];
+		const headingInFoldingStartLineNum : number[] = [-1, -1, -1];
 
 		console.log("FoldingRange analysis start");
-		const logRanges = (name: string) => console.log("> " + name + ": " + (foldingRanges[foldingRanges.length-1].start+1) + "-" + (foldingRanges[foldingRanges.length-1].end+1));
-		for(let i = 0; i < document.lineCount; i++){
-			const line = document.lineAt(i);
+		const logRanges = (name: string) => console.log(
+			"> " + name + ": " + (foldingRanges[foldingRanges.length-1].start+1) + "-" + (foldingRanges[foldingRanges.length-1].end+1)
+		);
+		for(let lineNum = 0; lineNum < document.lineCount; lineNum++){
+			const line = document.lineAt(lineNum);
 			const headingMatch = line.text.match(/^\*{0,3}/);
 			const headingLevel = headingMatch && headingMatch[0].length || 0;
 			if(headingLevel){
 				// Close opened deeper headings
-				for(let j = headingLevel; j <= 3; j++){
-					if(headingStartLine[j-1] !== -1){
-						foldingRanges.push(new vscode.FoldingRange(headingStartLine[j-1], i-1));
+				for(let level = headingLevel; level <= 3; level++){
+					if(headingStartLineNum[level-1] !== -1){
+						foldingRanges.push(new vscode.FoldingRange(headingStartLineNum[level-1], lineNum-1));
 						logRanges("Heading");
-						headingStartLine[j-1] = -1;
-						headingInFolding[j-1] = -1;
+						headingStartLineNum[level-1] = -1;
+						headingInFoldingStartLineNum[level-1] = -1;
 					}
 				}
 
-				headingStartLine[headingLevel-1] = i;
-				if(foldingStartLine.length){
-					headingInFolding[headingLevel-1] = foldingStartLine[foldingStartLine.length-1];
+				headingStartLineNum[headingLevel-1] = lineNum;
+				if(foldingStartLineNum.length){
+					headingInFoldingStartLineNum[headingLevel-1] = foldingStartLineNum[foldingStartLineNum.length-1];
 				}
 			} else if(line.text.match(/^\[(\+|\-)\]/)){
-				foldingStartLine.push(i);
+				foldingStartLineNum.push(lineNum);
 			} else if(line.text.match(/^\[END\]/)){
-				if(foldingStartLine.length){
-					let start = foldingStartLine.pop();
+				if(foldingStartLineNum.length){
+					let start = foldingStartLineNum.pop();
 					if(start !== undefined){
-						foldingRanges.push(new vscode.FoldingRange(start, i));
+						foldingRanges.push(new vscode.FoldingRange(start, lineNum));
 						logRanges("Folding");
 
 						// Close headings in this folding
-						for(let j = 1; j <= 3; j++){
-							if(headingInFolding[j-1] === start){
-								foldingRanges.push(new vscode.FoldingRange(headingStartLine[j-1], i-1));
+						for(let level = 1; level <= 3; level++){
+							if(headingInFoldingStartLineNum[level-1] === start){
+								foldingRanges.push(new vscode.FoldingRange(headingStartLineNum[level-1], lineNum-1));
 								logRanges("Heading");
-								headingStartLine[j-1] = -1;
-								headingInFolding[j-1] = -1;
+								headingStartLineNum[level-1] = -1;
+								headingInFoldingStartLineNum[level-1] = -1;
 							}
 						}
 					}
@@ -128,13 +130,13 @@ class FoldingRangeProvider {
 			}
 		}
 		for(const level of [1, 2, 3]){
-			if(headingStartLine[level-1] !== -1){
-				foldingRanges.push(new vscode.FoldingRange(headingStartLine[level-1], document.lineCount-1));
+			if(headingStartLineNum[level-1] !== -1){
+				foldingRanges.push(new vscode.FoldingRange(headingStartLineNum[level-1], document.lineCount-1));
 				logRanges("Heading");
 			}
 		}
-		while(foldingStartLine.length){
-			let start = foldingStartLine.pop();
+		while(foldingStartLineNum.length){
+			let start = foldingStartLineNum.pop();
 			if(start !== undefined){
 				foldingRanges.push(new vscode.FoldingRange(start, document.lineCount-1));
 				logRanges("Folding");
